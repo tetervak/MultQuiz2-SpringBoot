b374e7cd05d1d87499d01066ae14f4b59ef066a2
Alex Tetervak <tetervak@sheridancollege.ca>
2020-02-03 19:36:27 -0500

    initial commit

---
+++ src/main/java/ca/javateacher/multquiz2/MultQuiz2Application.java
@@ -0,0 +1,13 @@
+package ca.javateacher.multquiz2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultQuiz2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultQuiz2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ca/javateacher/multquiz2/MultQuiz2ApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.javateacher.multquiz2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultQuiz2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
