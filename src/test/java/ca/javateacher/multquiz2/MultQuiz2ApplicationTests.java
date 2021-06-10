bf168124d169c0fb7618d6ab539ade83d9b5cc1e
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-06-10 11:43:12 -0400

    fixed a compilation issue after updating the libs

--- src/test/java/ca/javateacher/multquiz2/MultQuiz2ApplicationTests.java
+++ src/test/java/ca/javateacher/multquiz2/MultQuiz2ApplicationTests.java
@@ -1,11 +1,9 @@
 package ca.javateacher.multquiz2;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class MultQuiz2ApplicationTests {
 
