6476aaeb22afb15cc48ff9f62246ed499f4bc1d0
Alex Tetervak <tetervak@sheridancollege.ca>
2023-06-04 23:09:50 -0400

    updated libs

--- src/main/java/ca/tetervak/multquiz2/MultQuizController.java
+++ src/main/java/ca/tetervak/multquiz2/MultQuizController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MultQuizController {
